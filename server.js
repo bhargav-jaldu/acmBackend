f72fad8180f4f764fc56811612dbd59455d6ac85
Bhargav Jaldu <bhargavjaldu868860@gmail.com>
2022-01-20 15:26:56 +0530

    initial

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const url = require('url');
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+const contactForm = require('./routes/contacForm')
+
+app.use('/', contactForm);
+
+app.listen('3001', () => {
+    console.log('Server running on port 3001')
+})
